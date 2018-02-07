a8e0c626cb85f6533b1f097dc00091087f478dec
Alexander Urizar <urizaraj@gmail.com>
2018-02-07 22:16:15 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,17 @@
 def fizzbuzz(int)
   by3 = (if int % 3 == 0)
   by5 = (if int % 5 == 0)
+
+  if by3 && by5
+    return "FizzBuzz"
+  end
+  elsif by3
+    return "Fizz"
+  end
+  elsif by5
+    return "Buzz"
+  end
+  else
+    return nil
+  end
+end
